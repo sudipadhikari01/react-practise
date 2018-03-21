9f6a632f3463e2e29d800c7fe4b62853fd481d24
sudipadhikari <sudip.adhikari@deerwalk.edu.np>
2018-03-21 18:19:37 +0545

    component as a class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Cartoon(props){
-	return <h1>Hello, {props.name}</h1>
+class Cartoon extends React.Component{
+	render(){
+		return <h1>Hello,{this.props.name}</h1>
+	}
 }
 
 ReactDOM.render(
-	<Cartoon name='Sudip' />,
+	<Cartoon name='Sudip'/>,
 	document.getElementById('root')
 );
